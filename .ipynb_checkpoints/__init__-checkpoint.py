24ce2ead5957f1d1cd6e26483720e027772df0a8
Serafim Urukov <serafim.urukov@gmail.com>
2021-04-25 11:29:59 +0000

    gitignore

--- .ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-"""Import file for project."""
-
-from .constants import constants
-from .building import Building
--- .ipynb_checkpoints/constants-checkpoint.py
+++
@@ -1,5 +0,0 @@
-"""Constants are described here."""
-from pathlib import Path
-
-JSON_DIR = "json"
-SRC_DIR = Path("data/raw") # source directory
